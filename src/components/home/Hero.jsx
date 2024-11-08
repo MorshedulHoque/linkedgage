73896a1e55708387bb3c15e0b0a4fc986ee12c14
unknown <asm21612@gmail.com>
2024-11-08 21:20:10 +0600

    Updated

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -9,24 +9,26 @@ import Carve from "./Carve/Carve";
 
 const Hero = () => {
   return (
-    <div className="relative min-h-fit bg-black">
+    <div className="relative min-h-fit bg-black bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
       {/* Hero Card */}
-      <div
-        className="absolute top-[40%] sm:top-[38%] md:top-[35%] lg:top-[31%] left-[50%] transform -translate-x-[54%] -translate-y-1/2 z-20 px-4 w-full max-w-sm sm:max-w-md md:max-w-xl lg:max-w-2xl"
-      >
-        <div className="absolute w-full bordered border-8 rounded-2xl shadow-2xl border-gray-200/25">
-          <img src={demo2} alt="" className="w-full h-auto object-cover rounded-2xl" />
+      <div className="absolute top-[43%] sm:top-[38%] md:top-[35%] lg:top-[35%] left-[54%] sm:left-[40%] md:left-[35%] lg:left-[50%] flex justify-center transform -translate-x-[54%] sm:-translate-x-[40%] md:-translate-x-[35%] lg:-translate-x-[50%] -translate-y-1/2 z-20 px-4 w-full max-w-sm sm:max-w-md md:max-w-xl lg:max-w-2xl">
+        <div className="absolute w-[80%] sm:w-1/2 md:w-1/3 lg:w-full h-auto border-8 rounded-2xl shadow-2xl border-gray-200/25">
+          <img
+            src={demo2}
+            alt=""
+            className="w-full h-auto object-cover rounded-2xl"
+          />
         </div>
       </div>
 
       {/* Bubbles */}
-      <div className="absolute top-10 left-5 sm:top-20 sm:left-10 md:top-40 md:left-60 z-10">
+      <div className="absolute top-10 left-5 sm:top-20 sm:left-10 md:top-40 md:left-60 z-20">
         <img src={BubbleLg} alt="" className="w-10 sm:w-20 md:w-36 lg:w-48" />
       </div>
       <div className="absolute top-5 right-5 sm:top-10 sm:right-10 md:top-20 md:right-40 z-10">
         <img src={BubbleMd} alt="" className="w-8 sm:w-16 md:w-28 lg:w-32" />
       </div>
-      <div className="absolute left-1/2 transform -translate-x-1/2 -translate-y-[20%] sm:-translate-y-[18%] md:-translate-y-[15%] lg:-translate-y-[10%] z-20">
+      <div className="absolute left-1/2 transform -translate-x-1/2 -translate-y-[20%] sm:-translate-y-[18%] md:-translate-y-[15%] lg:-translate-y-[10%] z-10">
         <img
           src={MiddleLight}
           alt="Middle Light"
@@ -39,7 +41,11 @@ const Hero = () => {
       </div>
 
       {/* Hero Text Section */}
-      <div className="relative z-10 px-4 py-4 sm:py-6 text-center flex flex-col justify-center items-center gap-2" data-aos="zoom-out" data-aos-duration="2000">
+      <div
+        className="relative z-10 px-4 py-4 sm:py-6 text-center flex flex-col justify-center items-center gap-2"
+        data-aos="zoom-out"
+        data-aos-duration="2000"
+      >
         <h1 className="text-xl sm:text-2xl md:text-5xl lg:text-6xl font-bold text-gray-50">
           Elevate Your LinkedIn <br /> <span>Conversations</span>
         </h1>
